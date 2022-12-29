b9820dd43641e932644e78fd6cf1c98807acf130
allen0927 <allen@abin.com.tw>
2022-12-29 18:48:07 +0800

    hw9_4

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,22 @@ import wsConnect from './wsConnect'
 
 mongo.connect();
 
-const app = express()
+import path from "path";
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
+
+
 const server = http.createServer(app)
 const wss = new WebSocket.Server({ server })
 const db = mongoose.connection
