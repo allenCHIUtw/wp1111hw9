e0ef056becc814be52948e2cc86c65dda3e47a82
allen0927 <allen@abin.com.tw>
2022-12-29 17:23:43 +0800

    hw9_2

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,15 @@ import mongo from './mongo'
 import wsConnect from './wsConnect'
 
 mongo.connect();
+const API_ROOT =
+  process.env.NODE_ENV === "production"
+    ? "/api"
+    : "http://localhost:4000/api";
 
+const WS_URL =
+  process.env.NODE_ENV === "production"
+    ? window.location.origin.replace(/^https*/, "ws")
+    : "ws://localhost:4000";
 const app = express()
 const server = http.createServer(app)
 const wss = new WebSocket.Server({ server })
@@ -27,3 +35,4 @@ const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`)
 });
+export const ws = new WebSocket(WS_URL);
